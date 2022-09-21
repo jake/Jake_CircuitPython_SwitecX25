f20ae539980733d92331d719a7ceacf478dc1669
Jacob Bijani <jake@users.noreply.github.com>
2022-09-21 11:15:22 -0400

    Update jake_switecx25.py

--- jake_switecx25.py
+++ jake_switecx25.py
@@ -9,26 +9,16 @@
 Port of Arduino SwitecX25 library
 
 
-* Author(s): Jacob Bijani
+* Author(s): Jacob Bijani; Guy Carpenter, Clearwater Software
 
 Implementation Notes
 --------------------
-
-**Hardware:**
-
-.. todo:: Add links to any specific hardware product page(s), or category page(s).
-  Use unordered list & hyperlink rST inline format: "* `Link Text <url>`_"
 
 **Software and Dependencies:**
 
 * Adafruit CircuitPython firmware for the supported boards:
   https://circuitpython.org/downloads
 
-.. todo:: Uncomment or remove the Bus Device and/or the Register library dependencies
-  based on the library's use of either.
-
-# * Adafruit's Bus Device library: https://github.com/adafruit/Adafruit_CircuitPython_BusDevice
-# * Adafruit's Register library: https://github.com/adafruit/Adafruit_CircuitPython_Register
 """
 
 # imports
